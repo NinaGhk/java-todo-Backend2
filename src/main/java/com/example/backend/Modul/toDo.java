fa72664e6307e9fe666d94738d1f99bd680b27c0
Nina Guo <ninaguo1305@gmail.com>
2022-09-19 03:18:24 +0800

    Add ToDo and Repo

---
+++ src/main/java/com/example/backend/Modul/toDo.java
@@ -0,0 +1,16 @@
+package com.example.backend.Modul;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class toDo {
+
+      private String id;
+      private String values;
+      private String status;
+}
---
+++ src/main/java/com/example/backend/Repository/toDoRepo.java
@@ -0,0 +1,5 @@
+package com.example.backend.Repository;
+
+public class toDoRepo {
+
+}
